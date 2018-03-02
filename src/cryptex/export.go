33524d1d766827e962af6236cde91d3097a65538
teddy <teddy@teddy.fyi>
2018-03-01 22:46:36 -0800

    Add export capabilities
    This function gets back an array of strings ready to be output,
    to be sourced in a shell to set environment variables.

---
+++ src/cryptex/export.go
@@ -0,0 +1,31 @@
+package cryptex
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/toolateforteddy/arbitrary/src/arbitrary"
+	"github.com/toolateforteddy/errortrace"
+)
+
+func FormatForShellExport(data interface{}) ([]string, error) {
+
+	flattened, err := arbitrary.FlattenWithJoiner(data, exportingJoiner)
+	if err != nil {
+		return nil, errortrace.Wrap(err)
+	}
+	retArr := make([]string, 0, len(flattened))
+	for k, v := range flattened {
+		retArr = append(
+			retArr,
+			fmt.Sprintf("%s=%s", k, v))
+	}
+	return retArr, nil
+}
+
+func exportingJoiner(arr []string) string {
+	for i, str := range arr {
+		arr[i] = strings.ToUpper(str)
+	}
+	return strings.Join(arr, "_")
+}
---
+++ src/cryptex/export_test.go
@@ -0,0 +1,56 @@
+package cryptex
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/toolateforteddy/arbitrary/src/arbitrary"
+)
+
+func anonimize(data interface{}) interface{} {
+	var anon interface{}
+	err := arbitrary.Hydrate(data, &anon)
+	if err != nil {
+		panic(err)
+	}
+	return anon
+}
+
+func TestFormatForShellExport(t *testing.T) {
+	type args struct {
+		data interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "easy",
+			args: args{
+				data: anonimize(
+					map[string]map[string]string{
+						"foo": map[string]string{
+							"bar": "baz",
+						},
+					},
+				),
+			},
+			want:    []string{"FOO_BAR=baz"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FormatForShellExport(tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FormatForShellExport() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FormatForShellExport() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
